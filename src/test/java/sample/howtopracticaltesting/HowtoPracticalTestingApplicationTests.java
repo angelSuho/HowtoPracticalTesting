338619d255ec6906b2a0552d64cc8af2fd8cd648
SoohoLee <hyeri0603@naver.com>
2023-05-18 13:49:23 +0900

    project init

---
+++ src/test/java/sample/howtopracticaltesting/HowtoPracticalTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.howtopracticaltesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtoPracticalTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
